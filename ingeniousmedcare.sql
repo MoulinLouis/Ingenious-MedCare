dc68ede890e8e58d6fee49a0eec5fd3335842254
MoulinLouis <l.moulin@lprs.fr>
2019-12-18 14:08:23 +0100

    Update ingeniousmedcare.sql
    Mise en place clés étrangères
    Insertion de données pour effectuer des tests

--- ingeniousmedcare.sql
+++ ingeniousmedcare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 09 déc. 2019 à 15:27
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Généré le :  mer. 18 déc. 2019 à 13:06
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -37,7 +37,14 @@ CREATE TABLE IF NOT EXISTS `doctor` (
   `firstName` varchar(255) COLLATE utf8_bin NOT NULL,
   `specialization` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `doctor`
+--
+
+INSERT INTO `doctor` (`id`, `name`, `firstName`, `specialization`) VALUES
+(1, 'Eric', 'Benenou', 'Generaliste');
 
 -- --------------------------------------------------------
 
@@ -47,17 +54,23 @@ CREATE TABLE IF NOT EXISTS `doctor` (
 
 DROP TABLE IF EXISTS `medicalstock`;
 CREATE TABLE IF NOT EXISTS `medicalstock` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `description` varchar(2555) COLLATE utf8_bin NOT NULL,
-  `indication` varchar(255) COLLATE utf8_bin NOT NULL,
-  `laboratory` varchar(255) COLLATE utf8_bin NOT NULL,
-  `composition` varchar(255) COLLATE utf8_bin NOT NULL,
-  `quantity` int(10) NOT NULL,
-  `cisCode` int(11) NOT NULL,
-  `idToxicity` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `classification` varchar(255) COLLATE utf8_bin NOT NULL,
+  `substance` varchar(255) COLLATE utf8_bin NOT NULL,
+  `excipient` varchar(255) COLLATE utf8_bin NOT NULL,
+  `conservation` varchar(255) COLLATE utf8_bin NOT NULL,
+  `toxicity` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `toxicity` (`toxicity`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `medicalstock`
+--
+
+INSERT INTO `medicalstock` (`id`, `nom`, `classification`, `substance`, `excipient`, `conservation`, `toxicity`) VALUES
+(2, 'DOLIPRANE 1000 mg cp', 'ANALGESIQUES,PARACETAMOL', 'Paracétamol', 'povidone, amidon prégélatinisé, sodium carboxyméthylamidon, talc, magnésium stéarate', 'Avant ouverture : durant 36 mois', 1);
 
 -- --------------------------------------------------------
 
@@ -88,7 +101,14 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `note` varchar(255) COLLATE utf8_bin NOT NULL,
   `familyDoctor` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `patient`
+--
+
+INSERT INTO `patient` (`id`, `name`, `firstName`, `birthDate`, `email`, `gender`, `address`, `city`, `postalCode`, `phoneNumber`, `homePhoneNumber`, `country`, `profession`, `civilStatus`, `socialSecurityNumber`, `mutualNumber`, `bloodGroup`, `pathology`, `note`, `familyDoctor`) VALUES
+(1, 'Louis', 'Moulin', '2000-12-26', 'l.moulin@lprs.fr', 'H', '79 rue d\'alsace', 'Tremblay', 93290, 659737458, 123456789, 'France', 'Développeur web', 'Célibataire', 1234511111, 1234567, 'O+', 'Aucune', 'Note adressé du médecin', 'Jean Dubois');
 
 -- --------------------------------------------------------
 
@@ -102,8 +122,16 @@ CREATE TABLE IF NOT EXISTS `planning` (
   `dateTimeStart` datetime NOT NULL,
   `dateTimeEnd` datetime NOT NULL,
   `doctorId` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`id`),
+  KEY `doctorId` (`doctorId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `planning`
+--
+
+INSERT INTO `planning` (`id`, `dateTimeStart`, `dateTimeEnd`, `doctorId`) VALUES
+(1, '2019-12-25 12:00:00', '2019-12-25 13:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -113,9 +141,18 @@ CREATE TABLE IF NOT EXISTS `planning` (
 
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
-  `id` int(10) NOT NULL,
-  `nameRole` varchar(255) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `idRole` int(10) NOT NULL AUTO_INCREMENT,
+  `nomRole` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idRole`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`idRole`, `nomRole`) VALUES
+(1, 'administratif'),
+(2, 'stock');
 
 -- --------------------------------------------------------
 
@@ -128,7 +165,18 @@ CREATE TABLE IF NOT EXISTS `toxicity` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `toxicity`
+--
+
+INSERT INTO `toxicity` (`id`, `libelle`) VALUES
+(1, 'Aucune'),
+(2, 'Légère'),
+(3, 'Moyenne'),
+(4, 'Dangereux'),
+(5, 'Tres dangereux');
 
 -- --------------------------------------------------------
 
@@ -145,15 +193,39 @@ CREATE TABLE IF NOT EXISTS `user` (
   `name` varchar(255) COLLATE utf8_bin NOT NULL,
   `firstName` varchar(255) COLLATE utf8_bin NOT NULL,
   `idRole` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`id`),
+  KEY `idRole` (`idRole`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id`, `login`, `email`, `password`, `name`, `firstName`, `idRole`) VALUES
-(1, 'qzd', 'l.moulin@lprs.fr', 'qzd', 'qzd', 'qzd', 2);
+(1, 's', 's@s', 's', 'Elodie', 'Surupi', 2),
+(2, 'a', 'a@a', 'a', 'Jean', 'Dupilon', 1);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `medicalstock`
+--
+ALTER TABLE `medicalstock`
+  ADD CONSTRAINT `fk_toxicity` FOREIGN KEY (`toxicity`) REFERENCES `toxicity` (`id`);
+
+--
+-- Contraintes pour la table `planning`
+--
+ALTER TABLE `planning`
+  ADD CONSTRAINT `fk_doctor` FOREIGN KEY (`doctorId`) REFERENCES `doctor` (`id`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_role` FOREIGN KEY (`idRole`) REFERENCES `role` (`idRole`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

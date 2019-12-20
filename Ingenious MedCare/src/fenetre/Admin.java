f76b30b6ab603e0101dcc6e02dc8b443fe655c10
MoulinLouis <l.moulin@lprs.fr>
2019-12-20 17:19:35 +0100

    Update Admin.java
    Mise à jour fenêtre pour afficher les tableaux :
    - Utilisateur
    - Patient
    - Stock

--- Ingenious MedCare/src/fenetre/Admin.java
+++ Ingenious MedCare/src/fenetre/Admin.java
@@ -1,5 +1,5 @@
 package fenetre;
-
+import SQL.*;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -7,6 +7,7 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 
 import SQL.UserManagement;
+import model.buildTableModel;
 
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
@@ -14,12 +15,16 @@ import javax.swing.JLayeredPane;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import java.awt.FlowLayout;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.table.TableModel;
 
 public class Admin {
 
 	private JFrame frmIngeniousMedcare;
-	private JTable table;
+	private JTable tableAllUser;
+	private JTable tableAllStock;
+	private JTable tableAllPatient;
 
 	/**
 	 * Launch the application.
@@ -87,48 +92,81 @@ public class Admin {
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Utilisateur", null, panel_1, null);
-		ArrayList listUser = UserManagement.getAllUser();
-		table = new JTable();
+		try {
+			tableAllUser = new JTable(buildTableModel.buildTableModel(UserManagement.getAllUser()));
+			tableAllUser.setEnabled(false);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
+					.addComponent(tableAllUser, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(Alignment.TRAILING, gl_panel_1.createSequentialGroup()
 					.addContainerGap(45, Short.MAX_VALUE)
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
+					.addComponent(tableAllUser, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		panel_1.setLayout(gl_panel_1);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Stock", null, panel_2, null);
+		
+		try {
+			tableAllStock = new JTable(buildTableModel.buildTableModel(StockManagement.getAllMedicalStock()));
+			tableAllStock.setEnabled(false);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 429, Short.MAX_VALUE)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(tableAllStock, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_2.setVerticalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 233, Short.MAX_VALUE)
+				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+					.addContainerGap(45, Short.MAX_VALUE)
+					.addComponent(tableAllStock, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		panel_2.setLayout(gl_panel_2);
 		
 		JPanel panel_3 = new JPanel();
 		tabbedPane.addTab("Patient", null, panel_3, null);
+		
+		try {
+			tableAllPatient = new JTable(buildTableModel.buildTableModel(PatientManagement.getAllPatient()));
+			tableAllPatient.setEnabled(false);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
 		gl_panel_3.setHorizontalGroup(
 			gl_panel_3.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 429, Short.MAX_VALUE)
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(tableAllPatient, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_3.setVerticalGroup(
 			gl_panel_3.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 233, Short.MAX_VALUE)
+				.addGroup(Alignment.TRAILING, gl_panel_3.createSequentialGroup()
+					.addContainerGap(45, Short.MAX_VALUE)
+					.addComponent(tableAllPatient, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		panel_3.setLayout(gl_panel_3);
 		frmIngeniousMedcare.getContentPane().setLayout(groupLayout);

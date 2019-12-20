cf817310c5bbbc3280e5c66161c1f0d269b121c7
MoulinLouis <l.moulin@lprs.fr>
2019-12-20 11:25:33 +0100

    Ajout tableau fenetre Admin

--- Ingenious MedCare/src/fenetre/Admin.java
+++ Ingenious MedCare/src/fenetre/Admin.java
@@ -5,14 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+
+import SQL.UserManagement;
+
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.JLayeredPane;
 import javax.swing.JLabel;
+import javax.swing.JTable;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
 
 public class Admin {
 
 	private JFrame frmIngeniousMedcare;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -80,22 +87,50 @@ public class Admin {
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Utilisateur", null, panel_1, null);
+		ArrayList listUser = UserManagement.getAllUser();
+		table = new JTable();
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 431, Short.MAX_VALUE)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 235, Short.MAX_VALUE)
+				.addGroup(Alignment.TRAILING, gl_panel_1.createSequentialGroup()
+					.addContainerGap(45, Short.MAX_VALUE)
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		panel_1.setLayout(gl_panel_1);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Stock", null, panel_2, null);
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 429, Short.MAX_VALUE)
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 233, Short.MAX_VALUE)
+		);
+		panel_2.setLayout(gl_panel_2);
 		
 		JPanel panel_3 = new JPanel();
 		tabbedPane.addTab("Patient", null, panel_3, null);
+		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
+		gl_panel_3.setHorizontalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 429, Short.MAX_VALUE)
+		);
+		gl_panel_3.setVerticalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 233, Short.MAX_VALUE)
+		);
+		panel_3.setLayout(gl_panel_3);
 		frmIngeniousMedcare.getContentPane().setLayout(groupLayout);
 	}
 }

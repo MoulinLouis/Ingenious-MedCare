117c72243556b628dd6b6c71803fe121f4848673
MoulinLouis <l.moulin@lprs.fr>
2019-12-20 17:18:27 +0100

    Create buildTableModel.java
    Création de la méthode buildTableModel pour pouvoir mettre en forme des tableaux à partir d'un ResultSet

---
+++ Ingenious MedCare/src/model/buildTableModel.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.sql.*;
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.table.*;
+
+import com.mysql.*;
+
+public class buildTableModel {
+	public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
+
+	    ResultSetMetaData metaData = rs.getMetaData();
+
+	    // names of columns
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add("abc");
+	    }
+
+	    // data of the table
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (rs.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(rs.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    return new DefaultTableModel(data, columnNames);
+
+	}
+}

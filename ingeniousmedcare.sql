2b8c8a10b253afcbe5740602f3c39dd5c911e3ab
Da Fonseca Damien <43819224+DaFonsecaDamien@users.noreply.github.com>
2020-01-20 13:37:47 +0100

    Correction fichier SQL

--- ingeniousmedcare.sql
+++ ingeniousmedcare.sql
@@ -1,13 +1,4 @@
-﻿-- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 19 jan. 2020 à 20:21
--- Version du serveur :  10.4.11-MariaDB
--- Version de PHP :  7.4.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+﻿SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
